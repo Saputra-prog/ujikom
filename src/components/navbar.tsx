c2b22799f11a20a4aa9e9e7473c2c608d845013b
Saputra-prog <m4t11928@gmail.com>
2025-01-23 10:41:10 +0700

    push

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,23 +1,76 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
 
-function navbar() {
+function Navbar() {
+  const pathname = usePathname();
   return (
     <div>
-      <nav className="bg-[#2E5077] px-16 py-8 flex justify space-x-6 z-10 relative text-white">
-        <span>logo</span>
+      <nav className="bg-[#2E5077] px-16 py-8 flex justify text-white fixed-top">
+        <div>
+          <Image
+            src="/img/y.png"
+            width={80}
+            height={80}
+            alt="logo kafe"
+            className="bg-white rounded-full"
+          />
+        </div>
+        <Link href="/home">
+          <div className="hover:bg-[#4DA1A9] rounded-lg px-2 py-2">
+            <button
+              className={`rounded-lg px-2 py-2 ${
+                pathname === "/" ? "bg-[#4DA1A9]" : "text-white"
+              }`}
+            >
+              Home
+            </button>
+          </div>
+        </Link>
         <Link href="/makanan">
-          <button>Daftar Makanan</button>
+          <div className="hover:bg-[#4DA1A9] rounded-lg px-2 py-2">
+            <button
+              className={`rounded-lg px-2 py-2 ${
+                pathname === "/makanan" ? "bg-[#4DA1A9]" : "text-white"
+              }`}
+            >
+              Daftar Makanan
+            </button>
+          </div>
         </Link>
-        <Link href={"/minuman"}>
-          <button>Daftar Minuman</button>
+        <Link href="/minuman">
+          <div className="hover:bg-[#4DA1A9] rounded-lg px-2 py-2">
+            <button
+              className={`rounded-lg px-2 py-2 ${
+                pathname === "/minuman" ? "bg-[#4DA1A9]" : "text-white"
+              }`}
+            >
+              Daftar Minuman
+            </button>
+          </div>
         </Link>
-        <Link href={"/about"}>
-          <button>Tentang kami</button>
+        <Link href="/about">
+          <div className="hover:bg-[#4DA1A9] rounded-lg px-2 py-2">
+            <button
+              className={`rounded-lg px-2 py-2 ${
+                pathname === "/about" ? "bg-[#4DA1A9]" : "text-white"
+              }`}
+            >
+              About
+            </button>
+          </div>
         </Link>
+        <div className=" rounded-lg px-2 py-2 translate-x-[800%]">
+          <button className="border rounded-lg px-2 py-2 bg-red-500">
+            logout
+          </button>
+        </div>
       </nav>
     </div>
   );
 }
 
-export default navbar;
+export default Navbar;
